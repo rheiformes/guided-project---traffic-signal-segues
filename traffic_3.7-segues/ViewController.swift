551f8f14dc4d2f3ce2f44e351c69c878d7873905
rheiformes <rhearai77@gmail.com>
2022-12-14 11:57:11 -0600

    finished

--- traffic_3.7-segues/ViewController.swift
+++ traffic_3.7-segues/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segueSwitch: UISwitch!
+    //@IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "yellow", sender: nil)
+        }
+        
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "green", sender: nil)
+        }
+        
+    }
+    //    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+//
+//    }
+    
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        segue.destination.navigationItem.title = textField.text
+//    }
 
 }
 
